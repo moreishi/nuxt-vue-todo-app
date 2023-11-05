80ba3a10ced37d3da52e8c51cea5d0528df30888
Caesar Ian Belza <moreishi@gmail.com>
2023-11-05 16:06:37 +0800

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <v-app>
+      <TasksComponent />
+    </v-app>
+  </div>
+</template>
+<style scoped>
+
+.v-application {
+  background: linear-gradient(rgba(62,175,124,1) 0%, rgba(44,62,80,1) 90%) !important;
+}
+</style>
